a941e3b2ee4ec3ed1aa08f2ca05e03f7a2ef5bc5
Josh Vlk <josh@vlkpack.com>
2025-06-03 09:34:37 -0400

    testing out array flow

--- src/Array.test.ts
+++ src/Array.test.ts
@@ -1,7 +1,7 @@
 import { expect } from "@std/expect/expect"
 import { Array } from "./Array.ts"
 import type { NonEmptyArray, ReadonlyNonEmptyArray } from "./Array.ts"
-import { pipe } from "./utility.ts"
+import { flow, pipe } from "./utility.ts"
 
 Deno.test("map", () => {
   const a1: ReadonlyArray<number> = [1, 2, 3]
@@ -50,4 +50,53 @@ Deno.test("at", () => {
   expect(
     pipe([1, 2, 3], Array.at(0)),
   ).toEqual({ _tag: "Some", value: 1 })
+})
+
+Deno.test("array flow", () => {
+  const arrayPipe: {
+    <T, U>(
+      fn: (t: T) => U,
+      preProcess: <A>(t: T | A) => t is T,
+    ): <X>(arr: Array<X>) => Array<U>
+    <T, U>(
+      fn: (t: T) => U,
+    ): (arr: Array<T>) => Array<U>
+  } = <T, U, A extends T>(
+    fn: (t: A) => U,
+    preProcess?: (t: T | A) => t is A,
+  ) =>
+  (arr: Array<A>) => {
+    const acc: Array<U> = []
+    arr.forEach((item) => {
+      if (preProcess && !preProcess(item)) {
+        return
+      }
+      acc.push(fn(item))
+    })
+    return acc
+  }
+
+  const incrementAndStringify = flow((x: number) => x + 1, (x) => String(x))
+
+  expect(pipe([1, 2, 3], arrayPipe(incrementAndStringify))).toEqual([
+    "2",
+    "3",
+    "4",
+  ])
+
+  const arr2 = pipe(
+    [1, 2, 3, "3", false],
+    arrayPipe(
+      incrementAndStringify,
+      (x: unknown): x is number => typeof x === "number",
+    ),
+  )
+
+  expect(
+    arr2,
+  ).toEqual([
+    "2",
+    "3",
+    "4",
+  ])
 })
